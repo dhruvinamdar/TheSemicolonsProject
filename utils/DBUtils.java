46b3db4f44570e52f9ddc0e67646bec8cae62716
Goelsim <simran.goel1998@gmail.com>
2021-09-18 21:33:25 +0530

    Utils Layer

---
+++ utils/DBUtils.java
@@ -0,0 +1,38 @@
+package com.orderprocessing.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtils {
+	private static Connection connection;
+
+	public static Connection getConnection() {
+		if (connection != null)
+			return connection;
+
+		else {
+			try {
+				if (connection == null) {
+					String url = "jdbc:mysql://localhost:3306/day11?useSSL=false";
+					String username = "root";
+					String password = "root";
+					DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+					connection = DriverManager.getConnection(url, username, password);
+					// connection.setAutoCommit(false);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return connection;
+		}
+	}
+
+	public static void closeMyConnection() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
