2fa5282e6fccf885d6f8418fda941e02b7f7f060
preeti09011999 <preeti09011999@gmail.com>
2021-09-20 22:35:27 +0530

    Added the front-end files

--- DBSchemaScript.sql
+++ DBSchemaScript.sql
@@ -1,17 +1,43 @@
-CREATE database ORDER_PROCESSING
-USE ORDER_PROCESSING
-CREATE TABLE `order_processing`.`company` (
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema order_processing
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema order_processing
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `order_processing` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `order_processing` ;
+
+-- -----------------------------------------------------
+-- Table `order_processing`.`company`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `order_processing`.`company` (
   `COMPANY_ID` VARCHAR(10) NOT NULL,
   `COMPANY_NAME` VARCHAR(50) NOT NULL,
   `COMPANY_ADDRESS_LINE1` VARCHAR(40) NOT NULL,
   `COMPANY_ADDRESS_CITY` VARCHAR(45) NOT NULL,
   `COMPANY_ADDRESS_STATE` VARCHAR(45) NOT NULL,
   `COMPANY_GST_NUMBER` VARCHAR(15) NOT NULL,
-   `COMPANY_PINCODE` VARCHAR(10) NOT NULL,
-  PRIMARY KEY (`COMPANY_ID`)
-  );
+  `COMPANY_PINCODE` VARCHAR(6) NOT NULL,
+  PRIMARY KEY (`COMPANY_ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-CREATE TABLE `order_processing`.`customer` (
+
+-- -----------------------------------------------------
+-- Table `order_processing`.`customer`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `order_processing`.`customer` (
   `CUSTOMER_ID` VARCHAR(10) NOT NULL,
   `CUSTOMER_NAME` VARCHAR(30) NOT NULL,
   `CUSTOMER_GST` VARCHAR(15) NOT NULL,
@@ -21,57 +47,98 @@ CREATE TABLE `order_processing`.`customer` (
   `CUSTOMER_EMAIL` VARCHAR(30) NOT NULL,
   `CUSTOMER_CONTACT` LONGTEXT NOT NULL,
   `CUSTOMER_PINCODE` VARCHAR(6) NOT NULL,
-  PRIMARY KEY (`CUSTOMER_ID`)
-);
+  PRIMARY KEY (`CUSTOMER_ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-CREATE TABLE `order_processing`.`product` (
-  `PRODUCT_ID` INT NOT NULL,
-  `PRODUCT_NAME` VARCHAR(20) NOT NULL,
-  `PRODUCT_PRICE` INT NOT NULL,
-  `PRODUCT_CATEGORY` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`PRODUCT_ID`)
-);
 
-CREATE TABLE `order_processing`.`order` (
-  `ORDER_ID` VARCHAR(10) NOT NULL,
-  `ORDER_DATE` DATE NOT NULL,
-  PRIMARY KEY (`ORDER_ID`));
+-- -----------------------------------------------------
+-- Table `order_processing`.`employee`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `order_processing`.`employee` (
+  `EMPLOYEE_ID` INT NOT NULL,
+  `EMPLOYEE_NAME` VARCHAR(30) NOT NULL,
+  `EMPLOYEE_PASSWORD` VARCHAR(15) NOT NULL,
+  `LAST_LOGGIN_DATETIME` DATETIME NOT NULL,
+  PRIMARY KEY (`EMPLOYEE_ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-ALTER TABLE `order_processing`.`company` 
-ADD COLUMN `COMPANY_PINCODE` VARCHAR(45) NULL AFTER `COMPANY_GST_NUMBER`;
 
-CREATE TABLE `order_processing`.`order_line` (
-  `ORDER_ID` VARCHAR(10) NOT NULL,
-  `CUSTOMER_SHIPPING_ADDRESS_LINE1` VARCHAR(40) NOT NULL,
-  `CUSTOMER_SHIPPING_ADDRESS_CITY` VARCHAR(45) NULL,
-  `CUSTOMER_SHIPPING_ADDRESS_STATE` VARCHAR(45) NULL,
-  PRIMARY KEY (`ORDER_ID`),
-  CONSTRAINT `CUSTOMER_ID`
-    FOREIGN KEY (`ORDER_ID`)
-    REFERENCES `order_processing`.`customer` (`CUSTOMER_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
-
-CREATE TABLE `order_processing`.`invoice` (
+-- -----------------------------------------------------
+-- Table `order_processing`.`invoice`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `order_processing`.`invoice` (
   `INVOICE_ID` VARCHAR(10) NOT NULL,
   `INVOICE_DATE` DATE NOT NULL,
   `TOTAL_GST_VALUE` INT NOT NULL,
   `TOATL_INVOICE_VALUE` INT NOT NULL,
   `INVOICE_STATUS` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`INVOICE_ID`)
-  );
-  
-CREATE TABLE `order_processing`.`product_line` (
+  PRIMARY KEY (`INVOICE_ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `order_processing`.`order`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `order_processing`.`order` (
+  `ORDER_ID` VARCHAR(10) NOT NULL,
+  `ORDER_DATE` DATE NOT NULL,
+  PRIMARY KEY (`ORDER_ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `order_processing`.`order_line`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `order_processing`.`order_line` (
+  `ORDER_ID` VARCHAR(10) NOT NULL,
+  `CUSTOMER_SHIPPING_ADDRESS_LINE1` VARCHAR(40) NOT NULL,
+  `CUSTOMER_SHIPPING_ADDRESS_CITY` VARCHAR(45) NULL DEFAULT NULL,
+  `CUSTOMER_SHIPPING_ADDRESS_STATE` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`ORDER_ID`),
+  CONSTRAINT `CUSTOMER_ID`
+    FOREIGN KEY (`ORDER_ID`)
+    REFERENCES `order_processing`.`customer` (`CUSTOMER_ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `order_processing`.`product`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `order_processing`.`product` (
+  `PRODUCT_ID` INT NOT NULL,
+  `PRODUCT_NAME` VARCHAR(20) NOT NULL,
+  `PRODUCT_PRICE` INT NOT NULL,
+  `PRODUCT_CATEGORY` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`PRODUCT_ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `order_processing`.`product_line`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `order_processing`.`product_line` (
   `PRODUCT_ID` VARCHAR(10) NOT NULL,
   PRIMARY KEY (`PRODUCT_ID`),
   CONSTRAINT `INVOICE_ID`
     FOREIGN KEY (`PRODUCT_ID`)
-    REFERENCES `order_processing`.`invoice` (`INVOICE_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
+    REFERENCES `order_processing`.`invoice` (`INVOICE_ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
-  
-  
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
