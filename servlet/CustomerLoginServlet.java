eb06c64a509890074d4f5269e412f165f4640afc
Tejal Lolap <tejallolap99@gmail.com>
2021-09-21 23:23:19 +0530

    first commit for Customer Login Servlet

---
+++ servlet/CustomerLoginServlet.java
@@ -0,0 +1,66 @@
+package com.orderprocessing.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.orderprocessing.service.CustomerService;
+import com.orderprocessing.service.CustomerServiceImpl;
+import com.orderprocessing.utility.Customer;
+
+/**
+ * Servlet implementation class CustomerServlet
+ */
+@WebServlet("/CustomerLoginServlet")
+public class CustomerLoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		response.setContentType("text/html");
+		String customerName = request.getParameter("customerName");
+		String customerId = request.getParameter("customerId");
+		String customerPassword = request.getParameter("password");
+		CustomerService cservice = new CustomerServiceImpl();
+		Customer customer = cservice.validateCustomer(customerName, customerId, customerPassword);
+		
+		   
+		if(customer!=null){
+			HttpSession  session=request.getSession();
+			session.setMaxInactiveInterval(20);
+			if(session.isNew()) {
+				System.out.print("new session");
+			}
+			else {
+				session.invalidate();
+				session=request.getSession(true);
+				 
+			}
+			session.setAttribute("customer", customer);
+			RequestDispatcher rd=request.getRequestDispatcher("displayOrders");
+			rd.forward(request, response);
+		}
+		else {
+			out.println("<h1>Please enter the correct credentials</h1>");
+			RequestDispatcher rd=request.getRequestDispatcher("login.jsp");
+			rd.include(request, response);
+			
+			
+		}
+		
+	}
+
+}
