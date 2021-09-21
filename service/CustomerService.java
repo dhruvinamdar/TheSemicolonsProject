9ec03ba7513849d0d6643a3309fbe35b79afe836
Tejal Lolap <tejallolap99@gmail.com>
2021-09-21 22:53:48 +0530

    -add show invoice method

--- service/CustomerService.java
+++ service/CustomerService.java
@@ -1,9 +1,24 @@
 package com.orderprocessing.service;
 
+import java.util.List;
+
+import com.orderprocessing.utility.Customer;
+import com.orderprocessing.utility.Order;
+
+// Customer Service interface
 public interface CustomerService {
+	
+	public Customer validateCustomer(String customerName, String customerId, String password);
 	
 	public List<Order> displayQuote();
 	
 	public List<Order> displayAllQuoteDetails();
+
+	public void changeQuoteStatus(Order orderObj);
+	
+	public List<Order> displayOrder();
+	
+	public List<Invoice> showInvoice();
+
 	
 }
--- service/CustomerServiceImpl.java
+++ service/CustomerServiceImpl.java
@@ -1,18 +1,48 @@
 package com.orderprocessing.service;
 
+import java.util.List;
+
+import com.orderprocessing.utility.Customer;
+import com.orderprocessing.utility.Order;
+import com.orderprocessing.utility.Invoice;
+import com.orderprocessing.dao.CustomerDao;
+import com.orderprocessing.dao.CustomerDaoImpl;
+
+// implementation of Customer Service layer
 public class CustomerServiceImpl implements CustomerService {
 	private CustomerDao cdao;
-	private Order orderObj;
 	
-	public CustomerService() {
-		cdao = new CustomerServiceImpl();
+	// default constructor
+	public CustomerServiceImpl() {
+		cdao = new CustomerDaoImpl();
 	}
 	
+	public Customer validateCustomer(String customerName, String customerId, String customerPassword) {
+		Customer customer = cdao.checkCredentials(customerName, customerId, customerPassword);
+		//System.out.println(customer);
+		return customer;
+	}
+
 	public List<Order> displayQuote() {
-		cdao.displayQuoteDetails(orderObj);
+		return cdao.displayQuoteDetails();
 	}
 	
 	public List<Order> displayAllQuoteDetails() {
-		cdao.displayDetailedQuote(orderObj);
+		return cdao.displayDetailedQuote();
 	}
+	
+	public void changeQuoteStatus(Order orderObj) {
+		cdao.setQuoteStatus(orderObj);
+	}
+	
+	public List<Order> displayOrder() {
+		return cdao.displayOrderDetails();
+	}
+	
+	public List<Invoice> showInvoice() {
+		cdao.displayOrderInvoice();
+	}
+
+	
+	
 }
