b2d8f4f16d2cdfe63a8969f665c0fb4b4bc8cc11
Goelsim <simran.goel1998@gmail.com>
2021-09-22 23:48:15 +0530

    Updated Customer Dao

--- dao/CustomerDao.java
+++ dao/CustomerDao.java
@@ -1,23 +1,13 @@
 package com.orderprocessing.dao;
 
-import java.util.List;
-
+import com.orderprocessing.exception.CustomerNotFoundException;
 import com.orderprocessing.beans.Customer;
-import com.orderprocessing.beans.Order;
 
-//Customer Dao interface
+// Customer Dao interface
 public interface CustomerDao {
 	
-	public Customer checkCredentials(String customerName, String customerId, String password);
+	public Customer checkCredentials(String customerLogin, String password);
 	
-	public List<Order> displayQuoteDetails();
-	
-	public List<Order> displayDetailedQuote();
-
-	public void setQuoteStatus(Order orderObj);
-
-	public List<Order> displayOrderDetails();
-
-	public List<Invoice> displayOrderInvoice();
+	public String getCustomer(String customerIdOrName) throws CustomerNotFoundException;
 
 }
--- dao/CustomerDaoImpl.java
+++ dao/CustomerDaoImpl.java
@@ -4,164 +4,104 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.orderprocessing.exception.CustomerNotFoundException;
 import com.orderprocessing.beans.Customer;
-import com.orderprocessing.beans.Order;
 import com.orderprocessing.utils.DBUtils;
 
 // implementation of Customer Dao layer
 public class CustomerDaoImpl implements CustomerDao {
-	
+
 	Connection conn = null;
 	PreparedStatement prepStatement;
 	LocalDate statusDate;
-	
+
+	private static final String GET_CUSTOMER_BY_ID_OR_BY_NAME = "Select * from customer where CUSTOMER_ID = ? OR CUSTOMER_NAME = ?";
+
 	public CustomerDaoImpl() {
 		conn = DBUtils.getConnection();
 		prepStatement = null;
 	}
-	
+
 	// method to validate customer login details
-	public Customer checkCredentials(String customerName, String customerId, String customerPassword) {
+	public Customer checkCredentials(String customerLogin, String customerPassword) {
 		String sql = "select * from customer where CUSTOMER_NAME=? or CUSTOMER_ID=? and CUSTOMER_PASSWORD=?";
 		try {
 			prepStatement = conn.prepareStatement(sql);
-			prepStatement.setString(1, customerName);
-			prepStatement.setString(2, customerId);
+			prepStatement.setString(1, customerLogin);
+			prepStatement.setString(2, customerLogin);
 			prepStatement.setString(3, customerPassword);
 			Customer customer = new Customer();
 			ResultSet rs = prepStatement.executeQuery(sql);
-			if(rs.next()) {
-				if(((rs.getString(1).equals(customerName)) || (rs.getString(1).equals(customerId))) && (rs.getString(2).equals(customerPassword))){
-					customer.setCustomerId(rs.getString(1));
-					customer.setCustomerName(rs.getString(2));
-					customer.setCustomerGST(rs.getString(3));
-					customer.setCustomerAddress(rs.getString(4));
-					customer.setCustomerCity(rs.getString(5));
-					customer.setCustomerEmail(rs.getString(6));
-					customer.setCustomerContact(rs.getString(7));
-					customer.setCustomerPincode(rs.getString(8));
-					customer.setCustomerPassword(rs.getString(9));
+			if (rs.next()) {
+				if (((rs.getString(1).equals(customerLogin)) || (rs.getString(1).equals(customerLogin)))
+						&& (rs.getString(2).equals(customerPassword))) {
+					customer.setCustomerId(rs.getString("CUSTOMER_ID"));
+					customer.setCustomerName(rs.getString("CUSTOMER_NAME"));
+					customer.setCustomerGST(rs.getString("CUSTOMER_GST"));
+					customer.setCustomerAddress(rs.getString("CUSTOMER_ADDRESS_LINE1"));
+					customer.setCustomerCity(rs.getString("CUSTOMER_ADDRESS_CITY"));
+					customer.setCustomerState(rs.getString("CUSTOMER_STATE"));
+					customer.setCustomerEmail(rs.getString("CUSTOMER_EMAIL"));
+					customer.setCustomerContact(rs.getString("CUSTOMER_CONTACT"));
+					customer.setCustomerPincode(rs.getString("CUSTOMER_PINCODE"));
 				}
 			}
-		return customer;
-			
+			return customer;
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-	
-	// method to display quote id, date, shipping cost and total order value
-	public List<Order> displayQuoteDetails() {
-		String sql = "select ORDER_ID, ORDER_DATE, SHIPPING_COST, TOTAL_ORDER_VALUE from testorder where STATUS='Pending'";
+
+	public String getCustomer(String customerIdOrName) throws CustomerNotFoundException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		// stmt = null;
+		ResultSet rs = null;
+		Customer customer = new Customer();
+		String customerToJson = null;
 		try {
-			prepStatement = conn.prepareStatement(sql);
-			List<Order> quoteList = new ArrayList<>();
-			ResultSet rs = prepStatement.executeQuery(sql);
-			while(rs.next()) {
-				Order quote = new Order();
-				quote.setOrderId(rs.getString(1));
-				quote.setOrderDate(rs.getDate(2));
-				quote.setShippingCost(rs.getFloat(3));
-				quote.setTotalOrderValue(rs.getFloat(4));
-				
-				quoteList.add(quote);
-			}
-			return quoteList;
-		}
-		catch(SQLException e) {
+			prepStatement = conn.prepareStatement(GET_CUSTOMER_BY_ID_OR_BY_NAME);
+			prepStatement.setString(1, customerIdOrName);
+			prepStatement.setString(2, customerIdOrName);
+			rs = prepStatement.executeQuery();
+			if (rs.next()) {
+				customer.setCustomerId(rs.getString("CUSTOMER_ID"));
+				customer.setCustomerName(rs.getString("CUSTOMER_NAME"));
+				customer.setCustomerGST(rs.getString("CUSTOMER_GST"));
+				customer.setCustomerAddress(rs.getString("CUSTOMER_ADDRESS_LINE1"));
+				customer.setCustomerCity(rs.getString("CUSTOMER_ADDRESS_CITY"));
+				customer.setCustomerState(rs.getString("CUSTOMER_STATE"));
+				customer.setCustomerEmail(rs.getString("CUSTOMER_EMAIL"));
+				customer.setCustomerContact(rs.getString("CUSTOMER_CONTACT"));
+				customer.setCustomerPincode(rs.getString("CUSTOMER_PINCODE"));
+
+				customerToJson = objectMapper.writeValueAsString(customer);
+				return customerToJson;
+			} else
+				return new String("");
+
+		} catch (SQLException ex) {
+
+			ex.printStackTrace();
+
+		} catch (JsonProcessingException e) {
 			e.printStackTrace();
-		}
-		return null; 
-	}
-
-	// method to display detailed quote 
-	public List<Order> displayDetailedQuote() {
-		String sql = "select * from testorder where STATUS='Pending'";
-		try {
-			prepStatement = conn.prepareStatement(sql);
-			List<Order> quoteList = new ArrayList<>();
-			ResultSet rs = prepStatement.executeQuery(sql);
-			while(rs.next()) {
-				quoteList.add(new Order(rs.getString(1), rs.getDate(2), rs.getString(3), rs.getFloat(4), rs.getFloat(5), rs.getString(6), rs.getString(7)));
-			}
-			return quoteList;
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-
-	// method to convert quote to order by changing status to approved
-	public void setQuoteStatus(Order orderObj) {
-		statusDate = LocalDate.now();
-		String sql = "update testorder set STATUS_DATE='" + statusDate + "', STATUS='Approved'";
-		
-		try {
-			prepStatement = conn.prepareStatement(sql);
-			int num = prepStatement.executeUpdate(sql);
-			System.out.println("Changed");
-			if(num > 0) {
-				conn.commit();
-			}
-			
-		}
-		catch(SQLException e) {
+		} finally {
 			try {
-				conn.rollback();
-			} catch (SQLException e1) {
-				e1.printStackTrace();
+				if (rs != null)
+					rs.close();
+				if (prepStatement != null)
+					prepStatement.close();
+			} catch (SQLException ex) {
+				ex.printStackTrace();
 			}
-			e.printStackTrace();
 		}
-		
-		
-	}
 
-	// method to display order id, date shipping cost, total order value and status
-	public List<Order> displayOrderDetails() {
-		String sql = "select ORDER_ID, ORDER_DATE, SHIPPING_COST, TOTAL_ORDER_VALUE, STATUS from testorder where STATUS='Approved' or STATUS='Completed'";
-		try {
-			prepStatement = conn.prepareStatement(sql);
-			List<Order> orderList = new ArrayList<>();
-			ResultSet rs = prepStatement.executeQuery(sql);
-			while(rs.next()) {
-				Order order = new Order();
-				order.setOrderId(rs.getString(1));
-				order.setOrderDate(rs.getDate(2));
-				order.setShippingCost(rs.getFloat(3));
-				order.setTotalOrderValue(rs.getFloat(4));
-				order.setStatus(rs.getString(5));
-				
-				orderList.add(order);
-			}
-			return orderList;
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
-		return null; 
-	}
-
-	// method to display invoice details
-	public Invoice displayOrderInvoice() {
-		String sql = " select invoice.ORDER_ID, invoice.INVOICE_ID, invoice.INVOICE_DATE, invoice.TOTAL_GST_VALUE, invoice.TOATL_INVOICE_VALUE, invoice.INVOICE_STATUS from invoice, testorder where testorder.ORDER_ID = invoice.ORDER_ID and current_date() > testorder.STATUS_DATE;";
-		try {
-			prepStatement = conn.prepareStatement(sql);
-			Invoice invoice;
-			ResultSet rs = prepStatement.executeQuery(sql);
-			invoice = new Invoice(rs.getString(1), rs.getDate(2), rs.getFloat(3), rs.getFloat(4), rs.getString(5), rs.getString(6));
-			return invoice;
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
-		return null;
+		return customerToJson;
 	}
 }
