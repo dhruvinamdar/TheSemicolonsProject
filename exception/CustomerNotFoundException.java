c946ee405efbb379472a6723bc3f16373bfe1501
Tejal Lolap <tejallolap99@gmail.com>
2021-09-24 00:22:17 +0530

    add comment

---
+++ exception/CustomerNotFoundException.java
@@ -0,0 +1,27 @@
+package com.orderprocessing.exception;
+
+/*
+ * User defined exception which handles invalid customer credentials
+ */
+public class CustomerNotFoundException extends Exception{
+	
+	public CustomerNotFoundException() {
+		super();
+	}
+
+	public CustomerNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public CustomerNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public CustomerNotFoundException(String message) {
+		super(message);
+	}
+
+	public CustomerNotFoundException(Throwable cause) {
+		super(cause);
+	}
+}
