cc3e859e338fcc9b52fbe5a03808e4353bc33028
VedantKP <vedant.puranik@gmail.com>
2021-09-24 20:36:18 +0530

    Working code for uploading file to server (Initial commit)

--- com/orderprocessing/servlet/FileUploadServlet.java
+++ com/orderprocessing/servlet/FileUploadServlet.java
@@ -1,43 +1,65 @@
 package com.orderprocessing.servlet;
 
 import java.io.File;
-
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+@MultipartConfig
 @WebServlet("/uploadFile")
-public class FileUploadServlet extends HttpServlet{
+public class FileUploadServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
-	public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException {
-		//Use session logic to check if logged in
-		
-		Part filePart = request.getPart("fileToUpload");
-		if(filePart.getSubmittedFileName().endsWith(".json"))
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			System.out.println("Inside file upload servlet");
+			Part filePart = request.getPart("jsonFile");
+			if(filePart.getSubmittedFileName().endsWith(".json"))
+			{
+				System.out.println(".json condition satisfied");
+			    InputStream fileInputStream = filePart.getInputStream();
+			    String path = this.getServletContext().getRealPath("" + filePart.getSubmittedFileName());
+			    File fileToSave = new File(path);
+			    System.out.println("Filepath in project is => " + path);
+				Files.copy(fileInputStream, fileToSave.toPath(), StandardCopyOption.REPLACE_EXISTING);
+				
+				//write function call to service
+
+				//Code to read JSON (remove when integrating) ---
+				Object obj = new JSONParser().parse(new FileReader(path));
+				JSONObject jo = null;
+				if(obj!=null)
+					jo = (JSONObject) obj;
+				if(jo!=null)
+					System.out.println("Json data => " + jo.toString());
+				//-----
+			}
+			else{
+				//Send response to front end with invalid extension of file (write necessary message in double quotes)
+				String errorMessage = "";
+				request.setAttribute("invalidFileError",errorMessage);
+				request.getRequestDispatcher("").forward(request, response);
+			}
+		}
+		catch(Exception exp)
 		{
-		    InputStream fileInputStream = filePart.getInputStream();
-		    //Path of file saving could give a problem
-		    File fileToSave = new File("WebContent/uploaded-files/" + filePart.getSubmittedFileName());
-			Files.copy(fileInputStream, fileToSave.toPath(), StandardCopyOption.REPLACE_EXISTING);
-			//String fileUrl = "http://localhost:8080/uploaded-files/" + filePart.getSubmittedFileName();			
-			request.setAttribute("filePath", fileToSave.toPath());
-			request.getRequestDispatcher("importProductsToDB").forward(request, response);
+			System.out.println("Exception => " + exp.getMessage());
 		}
-		else{
-			//Send response to front end with invalid extension of file
-			String errorMessage = "";
-			request.setAttribute("invalidFileError",errorMessage);
-			request.getRequestDispatcher("").forward(request, response);
-		}
+		
 	}
 }
