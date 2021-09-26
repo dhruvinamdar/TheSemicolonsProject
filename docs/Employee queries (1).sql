66ddba54a604cf659c758d0e784b9a7c4ad4f114
Dhruv Inamdar <63434590+dhruvinamdar@users.noreply.github.com>
2021-09-26 23:55:27 +0530

    Delete Employee queries (1).sql

--- docs/Employee queries (1).sql
+++
@@ -1,5 +0,0 @@
-insert into employee values('EL2359CLS', 'Pooja Pradhan', 'cofeyisi26', now());
-insert into employee values('ENL1GX1Z7', 'Shekhar S', 'wudazixe10', now());
-insert into employee values('E9JYZ6C9T', 'Tanya Ghosh', 'numoyume84', now());
-insert into employee values('E2E47PHFB', 'Sameer Raina', 'jidumxla95', now());
-insert into employee values('ER17YHW5F', 'Riya Joshi', 'pehzskda43', now());
