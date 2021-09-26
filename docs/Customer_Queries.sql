a47039a0c7546b488c097b52b3baed383f93dbcb
Dhruv Inamdar <63434590+dhruvinamdar@users.noreply.github.com>
2021-09-26 23:56:04 +0530

    Database Insertion Queries

---
+++ docs/Customer_Queries.sql
@@ -0,0 +1,16 @@
+insert into customer values('CQO4J04', 'Rekha Choudhary', '24HTMMP9872A1Z7', 'Kalupura Road, Gopal Bhuvan',
+'Vadodara', 'Gujarat', 'rekha@gmail.com', '7277103217', '390006',
+'muddycheese86');
+insert into customer values('C79RJHD', 'Padma Patil', '27WIBVT2265Q1Z5', '129 A, Mani Bhuvan, Vile Parle (east)',
+'Mumbai', 'Maharashtra', 'padma@gmail.com', '7480917473', '400057',
+'crunchyShapely');
+insert into customer values('CJZ7H4G', 'Rohan Gambhir', '07EIVNG1876C1Z4', '44, Ansari Market',
+'Delhi', 'Delhi', 'rohan@gmail.com', '9541163017', '110002',
+'tudorPious');
+insert into customer values('COVJJA0', 'Varun Shah', '19TUNVF7554W2Z9', '50/4 A, Banamali Sarkar St, Hatkhola',
+'Kolkata', 'West Bengal', 'varun@gmail.com', '8099701424', '700005',
+'angrilyBiscuit');
+insert into customer values('CP44RKE', 'Varsha Gupta', '29BHHEX8354M1Z2', '91/1, Near Akkipet Cross, Sultanpet',
+'Bangalore', 'Karnataka', 'varsha@gmail.com', '8351702431', '560053',
+'virgoSneer');
+
